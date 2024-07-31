f261cec2a9bc86f87442ff7f66d08c0f26ad64b8
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-07-31 14:19:13 -0500

    add code to center and style logo and text

--- SwingLogin/src/com/loginform/MainFrame.java
+++ SwingLogin/src/com/loginform/MainFrame.java
@@ -11,6 +11,7 @@ public class MainFrame
     private JPanel formPanel;
     private JLabel companyName;
     private ImageIcon icon;
+    private Font font;
 
     public MainFrame()
     {
@@ -23,24 +24,37 @@ public class MainFrame
         frame.setTitle("Login Page");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(1000, 500);
-        frame.setLayout(new BorderLayout(0, 0));
+        frame.setLayout(new BorderLayout());
         frame.setLocationRelativeTo(null);
 
         //set up main panel to add all components to later
         panel = new JPanel();
         panel.setLayout(new GridLayout(1, 2));
-        panel.setBackground(Color.GRAY);
+        //panel.setBackground(Color.LIGHT_GRAY);
 
         //set up components to add to logoPanel
         companyName = new JLabel("Rockwell Software");
-        icon = new ImageIcon("src/com/images/alien_outline.png");
+        companyName.setForeground(Color.WHITE);
 
+        //Icon set up
+        icon = new ImageIcon("src/com/images/alien_outline.png");
         companyName.setIcon(icon);
+
+        //Font setup
+        font = new Font("arial", Font.BOLD, 36);
+        companyName.setFont(font);
+
+        //set text and logo position
+        companyName.setHorizontalTextPosition(SwingConstants.CENTER);
+        companyName.setVerticalTextPosition(SwingConstants.BOTTOM);
+        companyName.setHorizontalAlignment(SwingConstants.CENTER);
+        companyName.setVerticalAlignment(SwingConstants.CENTER);
 
         //set up panel for left side
         logoPanel = new JPanel(new BorderLayout());
         logoPanel.setBackground(Color.DARK_GRAY);
         logoPanel.add(companyName);
+        
 
         //set up panel for right
         formPanel = new JPanel(new BorderLayout());
