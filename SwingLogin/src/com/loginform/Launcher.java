ba4a4c3e34b2e4b76f3ad9d6ffa42ca45ef90f75
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-07-31 12:01:22 -0500

    add code for basic Launcher class

---
+++ SwingLogin/src/com/loginform/Launcher.java
@@ -0,0 +1,10 @@
+package com.loginform;
+
+public class Launcher
+{
+    public static void main(String[] args)
+    {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.show();
+    }
+}
