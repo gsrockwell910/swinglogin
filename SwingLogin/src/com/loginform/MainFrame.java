1edb9c322bac1175f00bc51f2b77473eb814ceb4
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-07-31 18:25:38 -0500

    add code to set up text fields and add icons

--- SwingLogin/src/com/loginform/MainFrame.java
+++ SwingLogin/src/com/loginform/MainFrame.java
@@ -6,12 +6,27 @@ import java.awt.*;
 public class MainFrame
 {
     private JFrame frame;
+
     private JPanel panel;
     private JPanel logoPanel;
     private JPanel formPanel;
+    private JPanel formPanelWrapper;
+
     private JLabel companyName;
+    private JLabel textFieldLabel;
+    private JLabel passwordFieldLabel;
+
     private ImageIcon icon;
+    private ImageIcon textFieldIcon;
+    private ImageIcon passwordFieldIcon;
+
     private Font font;
+
+    private JTextField textField;
+    private JPasswordField passwordField;
+
+    private JButton loginButton;
+    private JButton resetButton;
 
     public MainFrame()
     {
@@ -30,7 +45,6 @@ public class MainFrame
         //set up main panel to add all components to later
         panel = new JPanel();
         panel.setLayout(new GridLayout(1, 2));
-        //panel.setBackground(Color.LIGHT_GRAY);
 
         //set up components to add to logoPanel
         companyName = new JLabel("Rockwell Software");
@@ -52,13 +66,66 @@ public class MainFrame
 
         //set up panel for left side
         logoPanel = new JPanel(new BorderLayout());
-        logoPanel.setBackground(Color.DARK_GRAY);
+        logoPanel.setBackground(new Color(74, 31, 61));
         logoPanel.add(companyName);
-        
+
+        //set up fields
+        textField = new JTextField(10);
+        textField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        textField.setBackground(new Color(186, 79, 84));
+        textField.setMargin(new Insets(0, 0, 0, 0));
+        textField.setForeground(Color.WHITE);
+
+        passwordField = new JPasswordField(10);
+        passwordField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        passwordField.setBackground(new Color(186, 79, 84));
+        passwordField.setForeground(Color.WHITE);
+
+        //set up text field logo
+        textFieldIcon = new ImageIcon("src/com/images/person_16.png");
+
+        //set up text field label
+        textFieldLabel = new JLabel(" ");
+        textFieldLabel.setIcon(textFieldIcon);
+        textFieldLabel.setIconTextGap(150);
+
+        //remove later if not needed
+        textFieldLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+        textFieldLabel.setVerticalTextPosition(SwingConstants.CENTER);
+        textFieldLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        textFieldLabel.setVerticalAlignment(SwingConstants.CENTER);
+
+        //set up password field logo
+        passwordFieldIcon = new ImageIcon("src/com/images/lock_16.png");
+
+        //set up password field label
+        passwordFieldLabel = new JLabel(" ");
+        passwordFieldLabel.setIcon(passwordFieldIcon);
+        passwordFieldLabel.setIconTextGap(150);
+
+        //remove later in not needed
+        passwordFieldLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+        passwordFieldLabel.setVerticalTextPosition(SwingConstants.CENTER);
+        passwordFieldLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        passwordFieldLabel.setVerticalAlignment(SwingConstants.CENTER);
 
         //set up panel for right
-        formPanel = new JPanel(new BorderLayout());
-        formPanel.setBackground(Color.ORANGE);
+        formPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 150));
+        formPanelWrapper = new JPanel(new GridLayout(2, 4));
+        formPanel.setBackground(new Color(186, 79, 84));
+        formPanelWrapper.setBackground(new Color(186, 79, 84));
+
+
+        //add text field components
+        formPanelWrapper.add(textFieldLabel);
+        formPanelWrapper.add(textField);
+
+        //add password
+        formPanelWrapper.add(passwordFieldLabel);
+        formPanelWrapper.add(passwordField);
+
+        //add wrapper
+        formPanel.add(formPanelWrapper);
 
         //add panel to frame
         panel.add(logoPanel);
