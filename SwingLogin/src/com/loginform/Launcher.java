6668d9a810978dbdfe14ef05a050ab77dd347847
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-07-31 18:43:08 -0500

    Add code to change look and feel

--- SwingLogin/src/com/loginform/Launcher.java
+++ SwingLogin/src/com/loginform/Launcher.java
@@ -1,10 +1,34 @@
 package com.loginform;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Launcher
 {
     public static void main(String[] args)
     {
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.show();
+        EventQueue.invokeLater(() ->
+        {
+            for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+            {
+                if("Metal".equals(info.getName()))
+                {
+                    try
+                    {
+                        UIManager.setLookAndFeel(info.getClassName());
+                    }
+                    catch(ClassNotFoundException |
+                          InstantiationException |
+                          IllegalAccessException |
+                          UnsupportedLookAndFeelException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.show();
+        });
     }
 }
