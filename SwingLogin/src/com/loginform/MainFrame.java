4e6f468817c8a90d2c1ac4276d5becb261f80415
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-07-31 12:25:47 -0500

    add code to style and add first panel to frame

--- SwingLogin/src/com/loginform/MainFrame.java
+++ SwingLogin/src/com/loginform/MainFrame.java
@@ -1,11 +1,18 @@
 package com.loginform;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 public class MainFrame
 {
     private JFrame frame;
+    private JPanel panel;
+    private JPanel logoPanel;
+    private JPanel formPanel;
+    private JLabel companyName;
+    private ImageIcon icon;
 
     public MainFrame()
     {
@@ -20,6 +27,14 @@ public class MainFrame
         frame.setSize(800, 500);
         frame.setLayout(new BorderLayout(10, 10));
         frame.setLocationRelativeTo(null);
+
+        //set up main panel to add all components to later
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.setBackground(Color.GRAY);
+
+        //add panel to frame
+        frame.add(panel, BorderLayout.CENTER);
     }
 
     public void show()
